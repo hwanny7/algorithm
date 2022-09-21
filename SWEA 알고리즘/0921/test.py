5928a52ac2824500703dfad8dfe6a818032f4818
hwanny7 <asdryzx2@gmail.com>
2022-09-21 23:53:09 +0900

    f

--- SWEA 알고리즘/0921/test.py
+++ SWEA 알고리즘/0921/test.py
@@ -1,5 +1,7 @@
 def find(idx, cnt):
     global grand
+    global cn
+    cn += 1
     if cnt == K or idx == l:
         total = 0
         for i in range(l):
@@ -10,24 +12,19 @@ def find(idx, cnt):
             grand = total
         return
 
-    count = 0
-    same = 0
     for i in range(l):
         if i == idx:
             continue
 
-        if arr[idx] == arr[i] and same == 0:
-            same += 1
+        if arr[idx] == arr[i] and i < idx:
             find(idx + 1, cnt + 1)
-        elif arr[idx] < arr[i] and idx < i:
-            count += 1
+        elif arr[idx] <= arr[i] and idx < i:
             arr[idx], arr[i] = arr[i], arr[idx]
             find(idx + 1, cnt + 1)
             arr[idx], arr[i] = arr[i], arr[idx]
 
     else:
-        if count == 0:
-            find(idx + 1, cnt)
+        find(idx + 1, cnt)
 
 
 for tc in range(1, int(input()) + 1):
@@ -36,6 +33,7 @@ for tc in range(1, int(input()) + 1):
     K = int(K)
     l = len(arr)
     grand = 0
+    cn = 0
     find(0, 0)
 
-    print(f'#{tc}', grand)
+    print(f'#{tc}', grand, cn)
