045852979306f74335add4eceea9097e8e3ac743
hwanny7 <asdryzx2@gmail.com>
2023-05-29 23:35:21 +0900

    플로이드 워셜 문제

--- ReStart/11403.py
+++ ReStart/11403.py
@@ -1,22 +1,37 @@
-def dfs(num):
-    print(num)
-    if visit[num] == 1:
-        return
-    visit[num] = 1
-
-    for i in range(N):
-        if arr[num - 1][i]:
-            arr[i][num - 1] = 1
-            dfs(i + 1)
 
 
+
+
+
+# N = int(input())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+#
+# for k in range(N):
+#     for i in range(N):
+#         for j in range(N):
+#             if arr[i][k] and arr[k][j]:
+#                 arr[i][j] = 1
+#
+# for i in range(N):
+#     print(*arr[i])
+
+def bfs(floor):
+    queue = [floor]
+    visit = [False] * N
+    while queue:
+        current_floor = queue.pop(0)
+        for i in range(N):
+            if arr[current_floor][i] and not visit[i]:
+                visit[i] = True
+                queue.append(i)
+                arr[floor][i] = 1
 
 
 N = int(input())
-visit = [0] * (N + 1)
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-dfs(1)
+for i in range(N):
+    bfs(i)
 
-for i in arr:
-    print(i)
+for i in range(N):
+    print(*arr[i])
